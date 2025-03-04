0b258b412591b39daf3a118bea6e88e1e5de04f6
Max <maxirobles1234@gmail.com>
2025-03-04 12:51:39 +0100

    add sections && navigation in home page

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -3,13 +3,21 @@ import React from "react";
 
 const Navbar = () => {
 	return (
-		<div className="flex items-center justify-between p-4 m-10">
+		<div className="flex items-center justify-between p-4 mx-10 sticky top-6">
 			<h1 className="text-2xl font-bold">Navbar</h1>
 			<div className="flex items-center gap-4">
-				<Link href="/">Profile</Link>
-				<Link href="/about">Showcases</Link>
-				<Link href="/about">Projects</Link>
-				<Link href="/contact">Contact</Link>
+				<a href="#profile" className="cursor-pointer">
+					Profile
+				</a>
+				<a href="#showcases" className="cursor-pointer">
+					Showcases
+				</a>
+				<a href="#projects" className="cursor-pointer">
+					Projects
+				</a>
+				<a href="#contact" className="cursor-pointer">
+					Contact
+				</a>
 				<button className="bg-blue-500 text-white px-4 py-2 rounded-md">
 					theme toggle
 				</button>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,37 @@
 import React from "react";
 
-const home = () => {
+const Home = () => {
 	return (
-		<div className="flex flex-col items-center justify-center h-screen">
-			<h1 className="text-4xl font-bold">Hello World</h1>
+		<div className="pt-16">
+			<section
+				id="profile"
+				className="h-screen flex flex-col items-center justify-center"
+			>
+				<h1 className="text-4xl font-bold">Profile Section</h1>
+			</section>
+
+			<section
+				id="showcases"
+				className="h-screen flex flex-col items-center justify-center"
+			>
+				<h1 className="text-4xl font-bold">Showcases</h1>
+			</section>
+
+			<section
+				id="projects"
+				className="h-screen flex flex-col items-center justify-center"
+			>
+				<h1 className="text-4xl font-bold">Projects</h1>
+			</section>
+
+			<section
+				id="contact"
+				className="h-screen flex flex-col items-center justify-center"
+			>
+				<h1 className="text-4xl font-bold">Contact</h1>
+			</section>
 		</div>
 	);
 };
 
-export default home;
+export default Home;
