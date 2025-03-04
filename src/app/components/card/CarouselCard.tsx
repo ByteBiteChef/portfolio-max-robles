1553d340e40024be293b379e36bbc3881ad0a923
Max <maxirobles1234@gmail.com>
2025-03-04 19:24:45 +0100

    add moking data to render a card with project

--- src/app/components/card/CarouselCard.tsx
+++ src/app/components/card/CarouselCard.tsx
@@ -2,28 +2,40 @@ import React from "react";
 
 interface CardData {
 	bgImage: string;
+	title: string;
 	stack1: string;
 	stack2: string;
 	stack3: string;
 	stack4: string;
+	description: string;
+	link: string;
 }
 
 const Card = ({ data }: { data: CardData }) => {
 	return (
-		<div className="relative w-90 h-60 overflow-hidden rounded-lg cursor-pointer group">
-			<div
-				className="absolute inset-0 bg-cover bg-center transition-transform duration-500 group-hover:scale-110"
-				style={{ backgroundImage: `url(${data.bgImage})` }}
-			></div>
+		<div className="raleway flex flex-col gap-4 w-90">
+			<div className="relative w-90 h-60 overflow-hidden rounded-lg group">
+				<div
+					className="absolute inset-0 bg-cover bg-center transition-transform duration-500 group-hover:scale-110"
+					style={{ backgroundImage: `url(${data.bgImage})` }}
+				></div>
 
-			<div className="absolute inset-0 bg-black opacity-0 transition-opacity duration-500 group-hover:opacity-70"></div>
+				<div className="absolute inset-0 bg-black opacity-0 transition-opacity duration-500 group-hover:opacity-70"></div>
 
-			<div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center opacity-0 group-hover:opacity-100 transition-opacity duration-500">
-				<p className="text-lg">{data.stack1}</p>
-				<p className="text-lg">{data.stack2}</p>
-				<p className="text-lg">{data.stack3}</p>
-				<p className="text-lg">{data.stack4}</p>
+				<div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+					<p className="text-lg">{data.stack1}</p>
+					<p className="text-lg">{data.stack2}</p>
+					<p className="text-lg">{data.stack3}</p>
+					<p className="text-lg">{data.stack4}</p>
+				</div>
 			</div>
+			<div>
+				<h2 className="text-lg font-bold">{data.title}</h2>
+				<p className="text-sm">{data.description}</p>
+			</div>
+			<a href={data.link} className="text-md">
+				View project
+			</a>
 		</div>
 	);
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,16 @@ import Card from "./components/card/CarouselCard";
 
 const Home = () => {
 	const cardData = {
+		title: "Algos.es",
 		bgImage:
 			"https://plus.unsplash.com/premium_photo-1683865776032-07bf70b0add1?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 		stack1: "React",
 		stack2: "Next.js",
 		stack3: "Tailwind CSS",
 		stack4: "TypeScript",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pulvinar et consectetur quam integer sit blandit nulla. Metus magna erat pretium a, tincidunt vitae.",
+		link: "https://algos.es",
 	};
 	return (
 		<div>
