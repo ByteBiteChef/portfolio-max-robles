e4ba9ca09ceeddbe46ffff01a80c7044825971a1
Max <maxirobles1234@gmail.com>
2025-03-04 13:59:10 +0100

    add logic to render a selected section botom border

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,25 +1,79 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 
 const Navbar = () => {
+	const [activeSection, setActiveSection] = useState("");
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const sections = ["profile", "showcases", "projects", "contact"];
+			let currentSection = "";
+
+			for (const section of sections) {
+				const element = document.getElementById(section);
+				if (element) {
+					const rect = element.getBoundingClientRect();
+					if (rect.top <= 150 && rect.bottom >= 150) {
+						currentSection = section;
+						break;
+					}
+				}
+			}
+
+			setActiveSection(currentSection);
+		};
+
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
 	return (
 		<div className="raleway flex items-center justify-between p-4 mx-10 sticky top-6">
 			<h1 className="text-2xl font-bold">Navbar</h1>
 			<div className="flex items-center gap-4">
-				<a href="#profile" className="cursor-pointer">
+				<a
+					href="#profile"
+					className={`cursor-pointer ${
+						activeSection === "profile"
+							? "border-b-2 border-[#A84E4F]"
+							: ""
+					}`}
+				>
 					Profile
 				</a>
-				<a href="#showcases" className=" cursor-pointer">
+				<a
+					href="#showcases"
+					className={`cursor-pointer ${
+						activeSection === "showcases"
+							? "border-b-2 border-[#A84E4F]"
+							: ""
+					}`}
+				>
 					Showcases
 				</a>
-				<a href="#projects" className="cursor-pointer">
+				<a
+					href="#projects"
+					className={`cursor-pointer ${
+						activeSection === "projects"
+							? "border-b-2 border-[#A84E4F]"
+							: ""
+					}`}
+				>
 					Projects
 				</a>
-				<a href="#contact" className="cursor-pointer">
+				<a
+					href="#contact"
+					className={`cursor-pointer ${
+						activeSection === "contact"
+							? "border-b-2 border-[#A84E4F]"
+							: ""
+					}`}
+				>
 					Contact
 				</a>
-				<button className="bg-blue-500 text-white px-4 py-2 rounded-md">
-					theme toggle
-				</button>
 			</div>
 		</div>
 	);
