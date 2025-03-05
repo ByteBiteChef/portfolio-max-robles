c5759cf7a0178cb06e1969d62e26eb3c37e1539e
Max <maxirobles1234@gmail.com>
2025-03-05 17:34:39 +0100

    add month list

--- src/components/githubContributions/GitHubContributions.tsx
+++ src/components/githubContributions/GitHubContributions.tsx
@@ -78,30 +78,65 @@ const GitHubContributions = () => {
 		weeks.push(contributions.slice(i, i + 7));
 	}
 
+	// Extract first day of each month for labels
+	const monthLabels: { name: string; index: number }[] = [];
+	contributions.forEach((day, index) => {
+		const month = new Date(day.date).toLocaleString("default", {
+			month: "short",
+		});
+		if (
+			monthLabels.length === 0 ||
+			monthLabels[monthLabels.length - 1].name !== month
+		) {
+			monthLabels.push({ name: month, index });
+		}
+	});
+
 	const years = Array.from(
 		{ length: 3 },
 		(_, i) => new Date().getFullYear() - i
 	);
 
 	return (
-		<div className="flex flex-col gap-4 w-full mt-10">
-			<h2 className="text-xl font-bold mb-4">My GitHub Contributions</h2>
+		<div className="flex flex-col w-full mt-10">
+			<h2 className="text-xl font-bold mb-4 font-raleway">
+				My GitHub Contributions.
+			</h2>
 
 			<div className="flex gap-4 w-full justify-between">
-				<div className="flex gap-1 border p-2 rounded-md items-center">
-					{weeks.map((week, weekIndex) => (
-						<div key={weekIndex} className="flex flex-col gap-1">
-							{week.map((day, dayIndex) => (
-								<div
-									key={dayIndex}
-									className={`w-4 h-4 rounded ${getColor(
-										day.contributionCount
-									)}`}
-									title={`${day.date}: ${day.contributionCount} contributions`}
-								></div>
-							))}
-						</div>
-					))}
+				<div className="flex flex-col border p-1 rounded-md">
+					<div className="flex text-xs font-medium text-gray-500 ml-10">
+						{monthLabels.map((month) => (
+							<div
+								key={month.name}
+								className="mr-6"
+								style={{
+									marginLeft: `${month.index * 0.25}px`,
+								}}
+							>
+								{month.name}
+							</div>
+						))}
+					</div>
+
+					<div className="flex gap-1  p-2">
+						{weeks.map((week, weekIndex) => (
+							<div
+								key={weekIndex}
+								className="flex flex-col gap-1"
+							>
+								{week.map((day, dayIndex) => (
+									<div
+										key={dayIndex}
+										className={`w-4 h-4 rounded ${getColor(
+											day.contributionCount
+										)}`}
+										title={`${day.date}: ${day.contributionCount} contributions`}
+									></div>
+								))}
+							</div>
+						))}
+					</div>
 				</div>
 
 				<div className="flex flex-col gap-2 text-sm w-1/2">
@@ -109,7 +144,7 @@ const GitHubContributions = () => {
 						<button
 							key={yr}
 							onClick={() => setYear(yr)}
-							className={`px-4 transition py-2 ${
+							className={`px-4 py-2 ${
 								year === yr
 									? "bg-blue-500 text-white rounded-sm"
 									: ""
