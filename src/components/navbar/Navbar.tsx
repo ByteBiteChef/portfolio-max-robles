9122f94bb6dc954340199cbb99d55a07f159deb1
Max <maxirobles1234@gmail.com>
2025-03-05 15:00:05 +0100

    fix bug in navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -32,53 +32,55 @@ const Navbar = () => {
 	}, []);
 
 	return (
-		<div className="raleway flex items-center justify-between p-12 sticky top-0">
-			<Link href="/" className="text-2xl roboto-slab">
-				Max
-				<br />
-				Robles.
-			</Link>
-			<div className="flex items-center gap-4">
-				<a
-					href="#profile"
-					className={`cursor-pointer ${
-						activeSection === "profile"
-							? "border-b-2 border-[#A84E4F] opacity-100"
-							: "opacity-50"
-					}`}
-				>
-					Profile
-				</a>
-				<a
-					href="#showcases"
-					className={`cursor-pointer ${
-						activeSection === "showcases"
-							? "border-b-2 border-[#A84E4F] opacity-100"
-							: "opacity-50"
-					}`}
-				>
-					Showcases
-				</a>
-				<a
-					href="#projects"
-					className={`cursor-pointer ${
-						activeSection === "projects"
-							? "border-b-2 border-[#A84E4F] opacity-100"
-							: "opacity-50"
-					}`}
-				>
-					Projects
-				</a>
-				<a
-					href="#contact"
-					className={`cursor-pointer ${
-						activeSection === "contact"
-							? "border-b-2 border-[#A84E4F] opacity-100"
-							: "opacity-50"
-					}`}
-				>
-					Contact
-				</a>
+		<div className="raleway flex sticky top-0">
+			<div className="flex items-center justify-between w-full m-10">
+				<Link href="/" className="text-2xl roboto-slab">
+					Max
+					<br />
+					Robles.
+				</Link>
+				<div className="flex items-center gap-4">
+					<a
+						href="#profile"
+						className={`cursor-pointer ${
+							activeSection === "profile"
+								? "border-b-2 border-[#A84E4F] opacity-100"
+								: "opacity-50"
+						}`}
+					>
+						Profile
+					</a>
+					<a
+						href="#showcases"
+						className={`cursor-pointer ${
+							activeSection === "showcases"
+								? "border-b-2 border-[#A84E4F] opacity-100"
+								: "opacity-50"
+						}`}
+					>
+						Showcases
+					</a>
+					<a
+						href="#projects"
+						className={`cursor-pointer ${
+							activeSection === "projects"
+								? "border-b-2 border-[#A84E4F] opacity-100"
+								: "opacity-50"
+						}`}
+					>
+						Projects
+					</a>
+					<a
+						href="#contact"
+						className={`cursor-pointer ${
+							activeSection === "contact"
+								? "border-b-2 border-[#A84E4F] opacity-100"
+								: "opacity-50"
+						}`}
+					>
+						Contact
+					</a>
+				</div>
 			</div>
 		</div>
 	);
