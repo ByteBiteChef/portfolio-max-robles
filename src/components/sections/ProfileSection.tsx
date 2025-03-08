e1463f58ce82946a1f5ca0796167a7e11c678a2f
Max Robles <maxirobles1234@gmail.com>
2025-03-08 01:31:16 +0100

    hide photo in mobile:

--- src/components/sections/ProfileSection.tsx
+++ src/components/sections/ProfileSection.tsx
@@ -2,7 +2,7 @@ import ScrollDown from "../scrollsown";
 import Image from "next/image";
 const ProfileSection = () => {
 	return (
-		<div className="raleway flex justify-center items-center h-screen w-full lg:flex-row flex-col mt-15">
+		<div className="raleway flex justify-start md:justify-center lg:justify-center items-center h-screen w-full lg:flex-row flex-col mt-15 md:mt-0 lg:mt-0 xl:mt-0">
 			<div className="flex max-w-[500px] flex-col gap-4 p-6">
 				<h1 className="font-raleway font-bold text-2xl">
 					Hello, <span className="block">My name is Max</span>
@@ -30,8 +30,8 @@ const ProfileSection = () => {
 					Get My Resume
 				</a>
 			</div>
-			<div className="relative isolate">
-				<div className="absolute top-30 left-30 w-120 h-120 bg-[#E8E6E6] hidden xl:block"></div>
+			<div className="relative isolate hidden xl:block">
+				<div className="absolute top-30 left-30 w-120 h-120 bg-[#E8E6E6] "></div>
 				<div className="w-80 h-80 lg:w-125 lg:h-125 xl:w-125 xl:h-125 bg-gray-400 flex justify-center items-center relative">
 					<Image src="/profile-photo.jpg" alt="Profile" fill />
 				</div>
