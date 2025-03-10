d4b1c2e7397b606600bc3176332f941764614c70
Max Robles <maxirobles1234@gmail.com>
2025-03-10 13:53:14 +0100

    add github nav card

--- src/components/sections/ShowcasesSection.tsx
+++ src/components/sections/ShowcasesSection.tsx
@@ -1,17 +1,40 @@
 import React from "react";
 import CarouselComponent from "../carousel/Carousel";
+import Image from "next/image";
 
 const ShowcasesSection = () => {
+	const handleNacClick = () => {
+		window.open("https://github.com/ByteBiteChef");
+	};
 	return (
-		<div className="flex flex-col items-center justify-center h-screen w-full md:w-2/3 lg:w-2/3 xl:w-2/3 px-6">
-			<div className="flex flex-col w-full py-8">
-				<h1 className="text-2xl font-bold">Showcases.</h1>
-				<p className="text-[#999999] w-full lg:w-1/2 ">
-					A selection of projects I&apos;ve worked on, from personal
-					builds to collaborations. Some are part of my professional
-					experience, while others, like my full-stack bootcamp final
-					project, were part of my learning process.
-				</p>
+		<div className="flex flex-col items-center justify-center h-screen w-full md:w-2/3 px-6">
+			<div className="flex w-full py-8 gap-6">
+				<div className="w-full lg:w-2/3 xl:w-2/3">
+					<h1 className="text-2xl font-bold">Showcases.</h1>
+					<p className="text-[#999999]  ">
+						A selection of projects I&apos;ve worked on, from
+						personal builds to collaborations. Some are part of my
+						professional experience, while others, like my
+						full-stack bootcamp final project, were part of my
+						learning process.
+					</p>
+				</div>
+				<button
+					onClick={handleNacClick}
+					className="hidden xl:flex justify-end items-center w-1/3 gap-4 px-4 cursor-pointer"
+				>
+					<Image
+						src="/profile-photo1.jpg"
+						alt="Profile"
+						width={500}
+						height={500}
+						className="rounded-full w-16 h-16 object-cover"
+					/>
+					<div className="flex flex-col items-start text-[#646464]">
+						<p className="roboto-slab">ByteBiteChef</p>
+						<p>Max Robles</p>
+					</div>
+				</button>
 			</div>
 			<div className="flex flex-col w-full">
 				<CarouselComponent />
